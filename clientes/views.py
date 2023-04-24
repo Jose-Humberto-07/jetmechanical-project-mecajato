2f4af3cbbadaced78961d4417bd971a3a3d659b8
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2023-04-23 22:02:02 -0300

    client app running normally on the server

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def clientes(request):
+    return HttpResponse('Estou no app clientes para teste, Hello Word')
