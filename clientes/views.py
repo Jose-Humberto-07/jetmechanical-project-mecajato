979d15369e0940b62067b2362864a453bf98fce2
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2023-05-08 23:01:58 -0300

    ajuste in view of app

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cliente, Carro
 from django.http import HttpResponse
 import re
 
@@ -7,7 +8,8 @@ import re
 
 def clientes(request):
     if request.method == 'GET':
-        return render(request, "clientes.html")
+        clientes_list = Cliente.objects.all()
+        return render(request, "clientes.html", {'clientes': clientes_list})
     elif request.method == 'POST':
         nome = request.POST.get('nome')
         sobrenome = request.POST.get('sobrenome')
@@ -23,9 +25,11 @@ def clientes(request):
 
         if cliente.exists():
             return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'email': email, 'carros': zip(carros, placas, anos) })
+            
 
         if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
             return render(request, 'clientes.html', {'nome': nome, 'sobrenome': sobrenome, 'cpf': cpf, 'carros': zip(carros, placas, anos)})
+            
 
 
 
