3f4bc2b41529852a5115ca2462dc4c97738042b2
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2023-09-25 20:46:36 -0300

    add new services and ajustes

--- servicos/forms.py
+++ servicos/forms.py
@@ -1,30 +1,24 @@
-from typing import Any, Dict, Mapping, Optional, Type, Union
-from django.core.files.base import File
-from django.db.models.base import Model
 from django.forms import ModelForm
-from django.forms.utils import ErrorList
-
 from .models import Servico, CategoriaManutencao
-
 
 class FormServico(ModelForm):
     class Meta:
         model = Servico
         exclude = ['finalizado', 'protocole']
 
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         for field in self.fields:
+            print(field)
             self.fields[field].widget.attrs.update({'class': 'form-control'})
             self.fields[field].widget.attrs.update({'placeholder': field})
+            
 
-        
         choices = list()
-
-        for i,j in self.fields['categoria_manutencao'].choices:
+        for i, j in self.fields['categoria_manutencao'].choices:
             categoria = CategoriaManutencao.objects.get(titulo=j)
             choices.append((i.value, categoria.get_titulo_display()))
+        
 
+        print(choices)
         self.fields['categoria_manutencao'].choices = choices
--- servicos/views.py
+++ servicos/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from .forms import FormServico
+from django.http import HttpResponse
+
+from .models import Servico
+
 # Create your views here.
 
 
 
 def novo_servico(request):
-    form = FormServico()
-    return render(request, 'novo_servico.html',{'form': form})
+    if request.method == "GET":
+        form = FormServico()
+        return render(request, 'novo_servico.html', {'form': form})
+    elif request.method == "POST":
+        form = FormServico(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return HttpResponse('Salvo com sucesso')
+        else:
+            return render(request, 'novo_servico.html', {'form': form})
+        
+        
+def listar_servico(request):
+    if request.method == "GET":
+        servicos = Servico.objects.all()
+        print(servicos)
+        return render(request, 'listar_servico.html', {'servicos': servicos})
+
+
+def servico(request, identificador):
+    servico = get_list_or_404(Servico, identificador=identificador)
+    print(servico)
+    return render(request, 'servico.html', {'servico': servico})
