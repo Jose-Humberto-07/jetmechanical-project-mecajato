01aa9e4df62fdd8cc3a7746196d810e3f85c4e9b
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-02-21 23:44:35 -0300

    add generate file in pdf

--- servicos/views.py
+++ servicos/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .forms import FormServico
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 
 from .models import Servico
+
+from fpdf import FPDF
+from io import BytesIO
 
 # Create your views here.
 
@@ -38,4 +41,31 @@ def servico(request, identificador):
 
 def gerar_os(request, identificador):
     servico = get_object_or_404(Servico, identificador=identificador)
-    return HttpResponse(identificador)
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font('Arial', 'B', 12)
+
+    pdf.set_fill_color(240,240,240)
+    pdf.cell(35, 10, 'Cliente:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.cliente.nome}', 1, 1, 'L', 1)
+
+    pdf.cell(35, 10, 'Manutenções:', 1, 0, 'L', 1)
+
+    categorias_manutencao = servico.categoria_manutencao.all()
+    for i, manutencao in enumerate(categorias_manutencao):
+        pdf.cell(0, 10, f'- {manutencao.get_titulo_display()}', 1, 1, 'L', 1)
+        if not i == len(categorias_manutencao) -1:
+            pdf.cell(35, 10, '', 0, 0)
+
+    pdf.cell(35, 10, 'Data de início:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.data_inicio}', 1, 1, 'L', 1)
+    pdf.cell(35, 10, 'Data de entrega:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.data_entrega}', 1, 1, 'L', 1)
+    pdf.cell(35, 10, 'Protocolo:', 1, 0, 'L', 1)
+    pdf.cell(0, 10, f'{servico.protocole}', 1, 1, 'L', 1)
+
+    pdf_content = pdf.output(dest='S').encode('latin1')
+    pdf_bytes = BytesIO(pdf_content)
+   
+    return FileResponse(pdf_bytes, as_attachment=True, filename=f"os-{servico.protocole}.pdf")
